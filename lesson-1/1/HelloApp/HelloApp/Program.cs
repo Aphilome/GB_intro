3813fa2f7148aef06310e9e63851fcb321f00acd
Aphilome <1.katrin.1@mail.ru>
2021-08-27 23:50:42 +0300

    Add HelloApp

---
+++ lesson-1/1/HelloApp/HelloApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            string name;
+            do
+            {
+                Console.Write("Введите свое имя: "); 
+                name = Console.ReadLine();
+            } while (name == "");
+            Console.WriteLine($"Привет, " + name + ", сегодня " + DateTime.Now.ToString("dd.MM.yyyy"));
+        }
+    }
+}
+
