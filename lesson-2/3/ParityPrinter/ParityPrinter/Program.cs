5f7dd7a2d24e4f458796f37289adbb5f7cc7008d
Aphilome <1.katrin.1@mail.ru>
2021-09-02 14:32:42 +0300

    Add ParityPrinter

---
+++ lesson-2/3/ParityPrinter/ParityPrinter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ParityPrinter
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Input an integer");
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                if (number % 2 == 0)
+                    Console.WriteLine("Number is even");
+                else
+                    Console.WriteLine("Number is odd");
+                return ;
+            }
+            Console.WriteLine("Error");
+        }
+    }
+}
