6a056de72d45fa56128f4e7fccd7111d8eb769de
Aphilome <1.katrin.1@mail.ru>
2021-09-04 23:05:58 +0300

    Add OlyaVsAyloPrinter

---
+++ lesson-3/3/OlyaVsAyloPrinter/OlyaVsAyloPrinter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace OlyaVsAyloPrinter
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            
+            Console.Write("Введите строку: ");
+            string input = Console.ReadLine();
+            while (string.IsNullOrWhiteSpace(input))
+            {
+                Console.Write("В строке отсутствуют данные. Введите строку: ");
+                input = Console.ReadLine();
+            }
+            var sb = new StringBuilder();
+            for (int i = input.Length - 1; i >= 0; i--)
+                sb.Append(input[i]);
+            Console.WriteLine($"Лексическая реверсия строки: {sb}");
+        }
+    }
+}
