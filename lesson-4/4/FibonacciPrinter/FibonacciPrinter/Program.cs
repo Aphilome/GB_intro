9041f74d9719e449ed7af43b720be1bcf981b4e9
Aphilome <1.katrin.1@mail.ru>
2021-09-05 02:41:52 +0300

    Add FibonacciPrinter

---
+++ lesson-4/4/FibonacciPrinter/FibonacciPrinter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FibonacciPrinter
+{
+    class Program
+    {
+        static ulong GetRecourseFibonacci(ulong index)
+        {
+            switch (index)
+            {
+                case 0:
+                    return 0;
+                case 1:
+                case 2:
+                    return 1;
+                default:
+                    return GetRecourseFibonacci(index - 1)
+                           + GetRecourseFibonacci(index - 2);
+            }
+        }
+        
+        static void Main()
+        {
+            Console.Write($"Input Fibonacci index: ");
+            string input = Console.ReadLine();
+            if (!ulong.TryParse(input, out ulong index))
+            {
+                Console.WriteLine("Error getting index");
+                return ;
+            }
+            Console.WriteLine($"Fibonacci ({index}) = {GetRecourseFibonacci(index)}");
+        }
+    }
+}
