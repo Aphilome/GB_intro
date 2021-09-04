da45b1bb494c5957deb2633ef171a8ecb55a4802
Aphilome <1.katrin.1@mail.ru>
2021-09-05 01:53:59 +0300

    Add SumPrinter

---
+++ lesson-4/2/SumPrinter/SumPrinter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SumPrinter
+{
+    class Program
+    {
+        static long Sum(string input)
+        {
+            long res = 0;
+            string[] nums = input.Split(' ');
+            foreach (var i in nums)
+            {
+                if (long.TryParse(i, out long tmpRes))
+                    res += tmpRes;
+            }
+            return res;
+        }
+        
+        static void Main()
+        {
+            Console.Write("Input numbers separated by a spaces: ");
+            string input = Console.ReadLine();
+            Console.WriteLine($"Sum = {Sum(input)}");
+        }
+    }
+}
